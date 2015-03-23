95e4cae5695b069eaf5caeaf97e15286876f986a
Yury Samokhvalov <ysamokhvalov@gmail.com>
2015-03-24 00:13:41 +1100

    first working version

--- superchart.py
+++ superchart.py
@@ -1,25 +1,19 @@
-#!/usr/bin/python
-
-import sys
 from lxml import html
-import requests
+import urllib
 
-
-def get_track_list():
-    page = requests.get("http://www.radiorecord.ru/radio/charts/")
-    tree = html.fromstring(page.text)
-    tracks = tree.xpath('//*[@id="page-superchart"]/div[3]')
-    return tracks
-
-def download_track():
-    pass
 
 def main():
-    tracks = get_track_list(body)
-    print tracks
-#    for track in tracks:
-#        download_track(track)
-#    print 'Hello world!'
+    url="http://www.radiorecord.ru/radio/charts/"
+    xpath_selector='//*[@id="page-superchart"]/div[@class="all_audio"]/article[@class="track-holder"]/div[@class="player-raiting-holder"]/div[@class="player_wrapper"]/table[@class="player"]/tr/td[@class="play_pause"]/@item_url'
+
+    page = html.parse(url)
+    tracks_urls = page.xpath(xpath_selector)
+    for url in tracks_urls[:3]:
+        idx = url.rfind('/')
+        filename = url[idx+1:]
+        print "Retriveing ", filename
+        urllib.urlretrieve(url, filename)
+
 
 if __name__ == '__main__':
     main()
